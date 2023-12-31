7a37ea99f929a5bef5a34a5aa92c4e168cc39634
babjiflaskapp <sk.valiya.babji@gmail.com>
2023-12-31 19:59:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Aside from './components/Aside'
 import Main from './components/Main'
-import AddNew from './components/AddNew'
 
 function App() {
   return (
